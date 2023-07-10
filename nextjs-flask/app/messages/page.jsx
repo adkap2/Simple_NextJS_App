f6933d8cd9868adafefa43920dd309a1f025f0d5
Adam Goldstein <adamgoldstein@Adams-MacBook-Pro-2.local>
2023-07-10 14:14:49 -0700

    second commit

---
+++ nextjs-flask/app/messages/page.jsx
@@ -0,0 +1,88 @@
+'use client'
+
+import React, { useState } from 'react';
+
+export default function Page() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevents the default form submission behavior
+
+    console.log('Form submitted');
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName);
+
+    // Create an object with the form data
+    const formData = {
+      firstName,
+      lastName
+    };
+
+    // Send the data to the server
+     fetch('https://jyu5m09x13.execute-api.us-west-1.amazonaws.com/prod', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response from the server if needed
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle any error that occurred during the request
+        console.error(error);
+      });
+
+  };
+  const handleReset = () => {
+    setFirstName('');
+    setLastName('');
+  };
+
+  return (
+    <form className="flex items-center justify-center h-screen" onSubmit={handleSubmit}>
+        <div className="flex flex-col">
+            <label htmlFor="fname" className="text-gray-700 font-bold">
+                First name:
+            </label>
+            <input
+                type="text"
+                id="fname"
+                name="fname"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"
+            />
+        </div>
+        <div className='flex flex-col'>
+            <label htmlFor="lname" className="text-gray-700 font-bold">
+                Last name:
+            </label>
+            <input
+                type="text"
+                id="lname"
+                name="lname"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"
+            />
+        </div>
+        <div className='flex-col'>
+            <br/>
+            <button type="submit" 
+                className="bg-blue-500 hover:bg-blue-700
+                 text-white font-bold py-2 px-4 rounded">
+                Submit
+            </button>
+            <button type="button" onClick={handleReset} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mb-0">                Reset
+            </button>
+        </div>
+    </form>
+
+  );
+
+}
---
+++ nextjs-flask/app/page.jsx
@@ -0,0 +1,45 @@
+/**
+
+Renders a Next.js page component that displays a grid of character avatars with links to individual character pages.
+@component
+@returns {JSX.Element} The rendered page component.
+*/
+
+import { Container } from '@/components'
+import Image from 'next/image'
+import Link from 'next/link'
+import { endpoint } from '@/utils/endpoint'
+
+
+
+async function getData() {
+  const res = await fetch('https://tll5qx2tb3.execute-api.us-west-1.amazonaws.com/prod/helloworld')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+ 
+export default async function Page() {
+  const data = await getData()
+  // Display data
+
+  return (
+
+    <main className="flex items-center justify-center h-screen">
+      <div>
+    <h1 className="block text-blue-900 text-sm font-bold mb-2 ">{data}</h1>
+      </div>
+      <div>
+        <Image src="/aws.png" alt='aws' width={70} height={50} />
+      </div>
+  </main>
+  )
+}
+
